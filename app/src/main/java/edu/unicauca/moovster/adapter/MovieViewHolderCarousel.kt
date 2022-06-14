de78407108501ccb9f5c472f872fe0eb27a9686f
Brayan Andres Pinzon Benavides <andrespinzon1414@gmail.com>
2022-06-14 11:30:21 -0500

    show information movie on click

--- app/src/main/java/edu/unicauca/moovster/adapter/MovieViewHolderCarousel.kt
+++ app/src/main/java/edu/unicauca/moovster/adapter/MovieViewHolderCarousel.kt
@@ -9,11 +9,17 @@ import com.jackandphantom.carouselrecyclerview.view.ReflectionImageView
 import edu.unicauca.moovster.R
 import edu.unicauca.moovster.movies.Movie
 
-class MovieViewHolderCarousel(view:View):RecyclerView.ViewHolder(view) {
+class MovieViewHolderCarousel(view:View, listener: MovieAdapterC.onItemClickListener):RecyclerView.ViewHolder(view) {
     val imageC = view.findViewById<ReflectionImageView>(R.id.image_carousel);
     fun render(movie:Movie){
         Glide.with(itemView)
             .load(movie.urlImage)
             .into(imageC);
+    }
+
+    init {
+        view.setOnClickListener{
+            listener.onItemClick(adapterPosition)
+        }
     }
 }
