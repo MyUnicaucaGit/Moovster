f2680dc926c6b52869811f0bb96d71d54903baf1
David Sotelo <juandavid.sotelo.m@gmail.com>
2022-06-16 17:46:53 -0500

    Login validate iniciated

--- app/src/main/java/edu/unicauca/moovster/ui/logIn/logIn.java
+++ app/src/main/java/edu/unicauca/moovster/ui/logIn/logIn.java
@@ -1,4 +1,4 @@
-package edu.unicauca.moovster.ui.navbar;
+package edu.unicauca.moovster.ui.logIn;
 
 import android.os.Bundle;
 
@@ -10,16 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import edu.unicauca.moovster.R;
-import edu.unicauca.moovster.ui.logIn.logIn;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NavBar#newInstance} factory method to
+ * Use the {@link logIn#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NavBar extends Fragment {
+public class logIn extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +32,7 @@ public class NavBar extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NavBar() {
+    public logIn() {
         // Required empty public constructor
     }
 
@@ -40,11 +42,11 @@ public class NavBar extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NavBar.
+     * @return A new instance of fragment logIn.
      */
     // TODO: Rename and change types and number of parameters
-    public static NavBar newInstance(String param1, String param2) {
-        NavBar fragment = new NavBar();
+    public static logIn newInstance(String param1, String param2) {
+        logIn fragment = new logIn();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,23 +66,26 @@ public class NavBar extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_bar, container, false);
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button btnUser = view.findViewById(R.id.btnNBUser);
-        btnUser.setOnClickListener(new View.OnClickListener() {
+        Button btnLogin = view.findViewById(R.id.btnLogIn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.nav_host_fragment_activity_main,new logIn())
-                        .commit();
+                    TextView txtEmail = view.findViewById(R.id.loginEmail);
+                    String tvEmail = txtEmail.getText().toString();
+                    TextInputLayout email = view.findViewById(R.id.loginEmailC);
+                    email.setHelperText("true");
+
+
             }
         });
-
     }
+
+
 }
