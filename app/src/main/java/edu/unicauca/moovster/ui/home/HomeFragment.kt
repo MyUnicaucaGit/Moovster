e65cea21576c0b63a47590fe79f222f7e06a4635
Brayan Andres Pinzon Benavides <andrespinzon1414@gmail.com>
2022-06-15 19:25:17 -0500

    fix multiply carousel load on back home

--- app/src/main/java/edu/unicauca/moovster/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/unicauca/moovster/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package edu.unicauca.moovster.ui.home
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
-import edu.unicauca.moovster.R
 import edu.unicauca.moovster.adapter.MovieViewHolder
 import edu.unicauca.moovster.databinding.FragmentHomeBinding
 import edu.unicauca.moovster.ui.carousel.CarouselFragment
@@ -17,6 +17,10 @@ class HomeFragment : Fragment() {
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<MovieViewHolder>? = null
     private var _binding: FragmentHomeBinding? = null
+    private var fragmentAnimacion: Fragment? = null;
+    private var fragmentAccion: Fragment? = null;
+    private var fragmentDrama: Fragment? = null;
+    private var accedido:Boolean=false;
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -40,20 +44,28 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Create new fragment
+        if (!accedido) {
+            accedido=true
+            fragmentAnimacion = CarouselFragment("Animación");
+            fragmentAccion = CarouselFragment("Acción");
+            fragmentDrama = CarouselFragment("Suspense");
 
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.add(edu.unicauca.moovster.R.id.containerHome,
+                    fragmentAnimacion as CarouselFragment
+                )
+                ?.add(edu.unicauca.moovster.R.id.containerHome, fragmentAccion as CarouselFragment)
+                ?.add(edu.unicauca.moovster.R.id.containerHome, fragmentDrama as CarouselFragment)
+                ?.commit();
+            accedido=false
+        }
 
-        val fragmentAnimacion: Fragment = CarouselFragment("Animación");
-        val fragmentAccion: Fragment = CarouselFragment("Acción");
-        val fragmentDrama: Fragment = CarouselFragment("Suspense");
-        activity?.supportFragmentManager?.beginTransaction()
-            ?.add(edu.unicauca.moovster.R.id.containerHome,fragmentAnimacion)
-            ?.add(edu.unicauca.moovster.R.id.containerHome,fragmentAccion)
-            ?.add(edu.unicauca.moovster.R.id.containerHome,fragmentDrama)
-            ?.commit();
     }
 }
