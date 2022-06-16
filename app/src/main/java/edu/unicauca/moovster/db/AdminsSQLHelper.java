2cb912c5a4475dad976347e54ffc1af03ea8840e
Brayan Andres Pinzon Benavides <andrespinzon1414@gmail.com>
2022-06-16 15:26:48 -0500

    Database and Fragment login or sing up

---
+++ app/src/main/java/edu/unicauca/moovster/db/AdminsSQLHelper.java
@@ -0,0 +1,34 @@
+package edu.unicauca.moovster.db;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AdminsSQLHelper extends SQLiteOpenHelper {
+
+
+    public AdminsSQLHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public AdminsSQLHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version, @Nullable DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase dbMoovster) {
+        dbMoovster.execSQL("CREATE TABLE User (  email String PRIMARY KEY,  pasword String NOT NULL,  name String, born_date String);");
+        dbMoovster.execSQL("CREATE TABLE Role ( roleName String PRIMARY KEY,  roleDescription String,  RoleTitle String,  roleImage String);");
+        dbMoovster.execSQL("CREATE TABLE Favorite_movies( movie_id INTEGER PRIMARY KEY, user_email String, FOREIGN KEY (user_email) REFERENCES User (email) ON DELETE CASCADE ON UPDATE NO ACTION);");
+        dbMoovster.execSQL("CREATE TABLE  User_Role( user_email String,  role String,  FOREIGN KEY (user_email) REFERENCES User (email) ON DELETE CASCADE ON UPDATE NO ACTION, FOREIGN KEY (role) REFERENCES Rol (email) ON DELETE CASCADE ON UPDATE NO ACTION);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
